a43415be905d5eb2799de44b68443a5c5c4592eb
Steve0Greatness <steve0greatnessiscool@gmail.com>
2025-02-10 22:16:59 -0800

    I have no idea how, but this works.

--- crates/footnote/src/main.rs
+++ crates/footnote/src/main.rs
@@ -5,7 +5,8 @@ use markdown_it::{
         block::{ BlockRule, BlockState },
         core::CoreRule,
         extset::MarkdownItExt
-    }
+    },
+    plugins::cmark::block::reference::ReferenceScanner
 };
 use std::collections::HashMap;
 
@@ -45,19 +46,11 @@ impl MarkdownItExt for FootnoteOptions {}
 struct FootnoteReference {
     pub Ref: String,
     pub Count: usize,
-
-    pub IdPref: String,
-    pub Class: String,
-    
-    pub DefIdPref: String,
 }
 #[derive(Debug)]
 struct FootnoteDefinition {
     pub Id: String,
     pub Count: usize, // The amount of references to the definition
-
-    pub IdPref: String,
-    pub Class: String,
 
     pub BRText: String,
     pub BRClass: String,
@@ -65,11 +58,7 @@ struct FootnoteDefinition {
     pub RefIdPref: String,
 }
 #[derive(Debug)]
-struct FootnoteList {
-    pub Content: Vec<(FootnoteDefinition, Node)>,
-
-    pub Class: String,
-}
+struct FootnoteList(usize);
 
 fn sluggify(name: &str) -> String {
     name.to_string().replace(|c| !char::is_alphanumeric(c) && c != ' ', "").replace(" ", "-").to_lowercase()
@@ -77,11 +66,7 @@ fn sluggify(name: &str) -> String {
 
 impl NodeValue for FootnoteReference {
     fn render(&self, node: &Node, fmt: &mut dyn Renderer) {
-        let footnote_definition = String::from("#") + &self.DefIdPref + "-" + &sluggify(&self.Ref);
-
-        let footnote_reference = String::new() + &self.IdPref + "-" + &sluggify(&self.Ref) + "-" + &self.Count.to_string();
-        
-        fmt.open("a", &[("href", footnote_definition), ("class", self.Class.clone()), ("id", footnote_reference)]);
+        fmt.open("a", &node.attrs);
         fmt.text(&self.Ref);
         fmt.close("a");
     }
@@ -90,13 +75,10 @@ impl NodeValue for FootnoteReference {
 
 impl NodeValue for FootnoteDefinition {
     fn render(&self, node: &Node, fmt: &mut dyn Renderer) {
-        let footnote_definition = String::new() + &self.IdPref + "-" + &sluggify(&self.Id);
-
-        let footnote_reference = String::from("#") + &self.RefIdPref + "-" + &sluggify(&self.Id) + "-";
-
-        fmt.open("li", &[("class", self.Class.clone()), ("id", footnote_definition)]);
+        fmt.open("li", &node.attrs);
+        fmt.cr();
         for i in 0..self.Count {
-            let fn_ref = footnote_reference.clone() + &i.to_string();
+            let fn_ref = self.RefIdPref.clone() + &i.to_string();
             fmt.open("a", &[("href", fn_ref), ("class", self.BRClass.clone())]);
             fmt.text_raw(&self.BRText);
             fmt.close("a");
@@ -105,17 +87,19 @@ impl NodeValue for FootnoteDefinition {
         fmt.text(&self.Id);
         fmt.close("strong");
         fmt.text(": ");
+        fmt.cr();
         fmt.contents(&node.children);
+        fmt.cr();
         fmt.close("li");
+        fmt.cr();
     }
 }
 
 impl NodeValue for FootnoteList {
     fn render(&self, node: &Node, fmt: &mut dyn Renderer) {
-        fmt.open("ul", &[("class", self.Class.clone())]);
-        for definition in self.Content.iter() {
-            definition.0.render(&definition.1, fmt);
-        }
+        fmt.open("ul", &node.attrs);
+        fmt.cr();
+        fmt.contents(&node.children);
         fmt.close("ul");
     }
 }
@@ -134,32 +118,30 @@ impl InlineRule for FootnoteRefsInlinRule {
         }
         
         let mut last_pos = 0;
-        for i in state.pos+2..input.len() {
+        for i in 2..input.len() {
             if !input[i..].starts_with("]") || input[i-1..].starts_with(r"\") {
                 continue;
             }
             last_pos = i;
             break;
         }
-        let label = String::from(&input[state.pos+2..last_pos]);
+        let label = String::from(&input[2..last_pos]);
 
         let options = state.md.ext.get::<FootnoteOptions>().unwrap();
 
-        let ref_id_pref = options.FootnoteRefIdPrefix.clone();
-        let ref_class   = options.FootnoteRefClassName.clone();
+        let ref_class = options.FootnoteRefClassName.clone();
 
         let def_id_pref = options.FootnoteDefIdPrefix.clone();
+        let mut node = Node::new(FootnoteReference {
+            Ref: label.clone(),
+            Count: 0,
+        });
+
+        node.attrs.push(("class", ref_class));
+        node.attrs.push(("href", String::from("#") + &def_id_pref + "-" + &sluggify(&label)));
         Some((
-            Node::new(FootnoteReference {
-                Ref: label,
-                Count: 0,
-
-                IdPref: ref_id_pref,
-                Class: ref_class,
-
-                DefIdPref: def_id_pref
-            }),
-            state.pos-last_pos
+            node,
+            last_pos+1
         ))
     }
 }
@@ -185,12 +167,6 @@ impl BlockRule for FootnoteDefsBlockRule {
     fn run(state: &mut BlockState) -> Option<(Node, usize)> {
         let label = Self::get_label(state)?;
 
-        let init_line = state.line;
-        state.blk_indent += 4;
-        state.md.block.tokenize(state);
-        state.blk_indent -= 4;
-        let content_lines = state.line - init_line;
-
         let options = state.md.ext.get::<FootnoteOptions>().unwrap();
 
         let def_id_pref = options.FootnoteDefIdPrefix.clone();
@@ -200,19 +176,39 @@ impl BlockRule for FootnoteDefsBlockRule {
         let br_class    = options.FootnoteBackRefCls.clone();
 
         let ref_id_pref = options.FootnoteRefIdPrefix.clone();
+        
+        let mut node = Node::new(FootnoteDefinition {
+            Id: label.clone(),
+            Count: 0,
+
+            BRText: br_text,
+            BRClass: br_class,
+
+            RefIdPref: ref_id_pref + "-" + &sluggify(&label) + "-"
+        }); 
+
+        node.attrs.push(("id", def_id_pref + "-" + &sluggify(&label)));
+        node.attrs.push(("class", def_class));
+
+        let old_node = std::mem::replace(&mut state.node, node);
+
+        let init_line = state.line;
+        let init_offsets = state.line_offsets[init_line].clone();
+
+        state.line_offsets[init_line].first_nonspace  += 5 + label.clone().len();
+        state.line_offsets[init_line].indent_nonspace += 5i32;
+        
+        state.blk_indent += 4;
+        state.md.block.tokenize(state);
+        state.blk_indent -= 4;
+        
+        let content_lines = state.line - init_line;
+        
+        state.line = init_line;
+        state.line_offsets[init_line] = init_offsets; 
+        
         Some((
-            Node::new(FootnoteDefinition {
-                Id: label,
-                Count: 0,
-
-                IdPref: def_id_pref,
-                Class: def_class,
-
-                BRText: br_text,
-                BRClass: br_class,
-
-                RefIdPref: ref_id_pref
-            }),
+            std::mem::replace(&mut state.node, old_node),
             content_lines
         ))
     }
@@ -222,8 +218,10 @@ impl CoreRule for FootnoteCountCoreRule {
     fn run(root: &mut Node, md: &MarkdownIt) {
         let mut counts: HashMap<String, usize> = HashMap::new();
 
+        let options = md.ext.get::<FootnoteOptions>().unwrap();
+        let footnote_reference  = |id: &str, count: usize| options.FootnoteRefIdPrefix.clone() + "-" + &sluggify(id) + "-" + &count.to_string();
         root.walk_mut(|node, _| {
-            let mut reference = match node.cast_mut::<FootnoteReference>() {
+            let reference = match node.cast_mut::<FootnoteReference>() {
                 Some(r) => r,
                 None    => return ()
             };
@@ -231,9 +229,11 @@ impl CoreRule for FootnoteCountCoreRule {
             counts.entry(ref_id.clone()).and_modify(|c| *c += 1).or_insert(1);
             let count = counts[&ref_id];
             reference.Count = count;
+
+            node.attrs.push(("id", footnote_reference(&ref_id, count)));
         });
         root.walk_mut(|node, _| {
-            let mut definition = match node.cast_mut::<FootnoteDefinition>() {
+            let definition = match node.cast_mut::<FootnoteDefinition>() {
                 Some(r) => r,
                 None    => return ()
             };
@@ -241,16 +241,80 @@ impl CoreRule for FootnoteCountCoreRule {
             counts.entry(def_id.clone()).or_insert(0);
             let count = counts[&def_id];
             definition.Count = count;
+
         });
     }
 }
 
+#[derive(Debug)]
+struct PlaceholderNode();
+impl NodeValue for PlaceholderNode {}
+
 impl CoreRule for FootnoteGroupCoreRule {
     fn run(root: &mut Node, md: &MarkdownIt) {
+        let mut index = 0;
+        let mut open_index = 0;
         let options = md.ext.get::<FootnoteOptions>().unwrap();
-        
-        let deflist_class = options.FootnoteDefListClassName.clone();
+        let class = options.FootnoteDefListClassName.clone();
+        let mut defs: Vec<(usize, Box<Node>)> = Vec::new();
+        root.walk_mut(|node, _depth| {
+            if !node.is::<FootnoteDefinition>() {
+                index += 1;
+                open_index = index;
+                return;
+            }
+            let mut replaced = if index != open_index { Node::new(PlaceholderNode()) } else { Node::new(FootnoteList(open_index)) };
+            replaced.attrs.push(("class", class.clone()));
+            let extract = std::mem::replace(node, replaced);
+            defs.push((open_index, Box::new(extract)));
+        });
+        index = 0;
+        root.children.retain(|child| {
+            if !child.is::<PlaceholderNode>() {
+                return true;
+            }
+            index += 1;
+            false
+        });
+        root.walk_mut(|node, _depth| {
+            let def_list_id = match node.cast_mut::<FootnoteList>() {
+                Some(FootnoteList(n)) => *n,
+                None => return (),
+            };
+            for def in &mut defs {
+                if def.0 != def_list_id { continue; }
+                let a = std::mem::replace(&mut def.1, Box::new(Node::new(PlaceholderNode())));
+                node.children.push(*a);
+            }
+        });
     }
 }
 
-fn main() {}
+fn add(md: &mut MarkdownIt) {
+    md.ext.get_or_insert_default::<FootnoteOptions>();
+    md.inline.add_rule::<FootnoteRefsInlinRule>();
+    md.block.add_rule::<FootnoteDefsBlockRule>().before::<ReferenceScanner>();
+    md.add_rule::<FootnoteCountCoreRule>();
+    md.add_rule::<FootnoteGroupCoreRule>().after::<FootnoteCountCoreRule>();
+}
+
+fn add_with_options(md: &mut MarkdownIt, options: FootnoteOptions) {
+    md.ext.insert(options);
+    md.inline.add_rule::<FootnoteRefsInlinRule>();
+    md.block.add_rule::<FootnoteDefsBlockRule>().before::<ReferenceScanner>();
+    md.add_rule::<FootnoteCountCoreRule>();
+    md.add_rule::<FootnoteGroupCoreRule>().after::<FootnoteCountCoreRule>();
+}
+
+fn main() {
+    let markdown = r#"Hello![^foo]
+
+[^foo]: bar!
+    Newline or *something*
+"#;
+    let parser = &mut MarkdownIt::new();
+    markdown_it::plugins::cmark::add(parser);
+    add(parser);
+    let testing = parser.parse(markdown).render();
+    println!("{}", testing);
+}
